076f04898d8396682bdd2156e0f0805dbc074f37
JordanLDiaz <jordanldiaz29@gmail.com>
2023-08-03 16:56:18 -0600

    update

--- app/AppState.js
+++ app/AppState.js
@@ -9,13 +9,13 @@ class AppState extends EventEmitter {
   values = loadState('values', [Value])
 
   /** @type {import('./Models/Car').Car[]} */
-  // cars = [
-  //   new Car({ make: 'Toyota', model: 'Tacoma', year: 2025, color: '#000', description: 'Yo', img: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', price: 10000 }),
-  //   new Car({ make: 'Subaru', model: 'Tsunami', year: 2025, color: '#000', description: 'Its cool', img: 'https://images.unsplash.com/photo-1616804087673-cdcd32e5578f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3ViYXJ1fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', price: 30000 })
-  // ]
+  cars = [
+    new Car({ make: 'Toyota', model: 'Tacoma', year: 2025, color: '#000', description: 'Yo', img: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', price: 10000 }),
+    new Car({ make: 'Subaru', model: 'Tsunami', year: 2025, color: '#000', description: 'Its cool', img: 'https://images.unsplash.com/photo-1616804087673-cdcd32e5578f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3ViYXJ1fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', price: 30000 })
+  ]
 
   // Once we're ready to save data to local storage, change fake car data above to loadstate below
-  cars = loadState('cars', [Car])
+  // cars = loadState('cars', [Car])
 }
 
 export const appState = new Proxy(new AppState(), {
